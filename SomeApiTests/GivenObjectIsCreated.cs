16aac9b05f41ce88e3064c097ecdaed5fc885968
Milosz Dura <milosz.md@wp.pl>
2016-12-17 18:15:35 +0000

    addded tests project

---
+++ SomeApiTests/GivenObjectIsCreated.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SomeApiTests
+{
+    [TestFixture]
+    public class GivenObjectIsCreated
+    {
+        public Person _subject;
+
+        [SetUp]
+        public void WhenTheNameIsSet()
+        {
+            _subject = new Person
+            {
+                Name = "Milosz",
+                DateOfBirth = new DateTime(1996, 10, 10)
+            };
+        }
+
+        [Test]
+        public void ThenTheNameIsSetCorrectly()
+        {
+            Assert.That(_subject.Name, Is.EqualTo("Milosz"));
+        }
+
+        [Test]
+        public void ThenTheDateOfBirthIsSetCorrectly()
+        {
+            Assert.That(_subject.Name, Is.EqualTo(new DateTime(1996, 10, 10)));
+        }
+    }
+}
